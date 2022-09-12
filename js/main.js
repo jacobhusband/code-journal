1644d61a5cefccb3c103de3f43ee7f1cf0302536
Jacob Husband <jakehusband2@gmail.com>
2022-09-12 21:22:56 +0000

    Make user create an entry feature for issue #1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 var $form = document.querySelector('form');
 var $photoUrl = $form.querySelector('.photo-url');
 var $entryImage = document.querySelector('.entry-image');
-var formDataObj = {};
+
+pushLocalStorage();
 
 $photoUrl.addEventListener('input', updateSrc);
 
 $form.addEventListener('submit', submitEntryForm);
 
+window.addEventListener('beforeunload', addToLocalStorage);
+
 function updateSrc(event) {
-  $entryImage.setAttribute('src', event.target.value);
+  if (isValidUrl(event.target.value)) {
+    $entryImage.setAttribute('src', event.target.value);
+  }
 }
 
 function submitEntryForm(event) {
   event.preventDefault();
+  var formDataObj = {};
 
   formDataObj.title = $form.elements.title.value;
   formDataObj.url = $form.elements.url.value;
@@ -22,4 +28,32 @@ function submitEntryForm(event) {
   data.entries.push(formDataObj);
   $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
+}
+
+function addToLocalStorage(event) {
+  localStorage.setItem('data-entries', JSON.stringify(data.entries));
+}
+
+function pushLocalStorage() {
+  var dataEntries = JSON.parse(localStorage.getItem('data-entries'));
+  if (dataEntries) {
+    dataEntries.forEach(entry => {
+      data.entries.push(entry);
+    });
+  }
+}
+
+// Grabbed a url validator off stackoverflow https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+function isValidUrl(urlString) {
+  var urlPattern = new RegExp(
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  );
+
+  return !!urlPattern.test(urlString);
 }
