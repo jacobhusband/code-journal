532de8ec6978721107eebf74a64b8eda386127a2
Jacob Husband <jakehusband2@gmail.com>
2022-09-13 00:41:07 +0000

    Take a single entry object and returns a DOM tree that matches an example entry

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 var $form = document.querySelector('form');
 var $entryImage = document.querySelector('.entry-image');
 var $photoUrl = document.querySelector('.photo-url');
+var $ul = document.querySelector('ul.entry-list');
 
 $form.addEventListener('submit', submitEntryForm);
 $photoUrl.addEventListener('input', updateSrc);
+
+var entry = createEntryElements({
+  title: 'Mona Lisa',
+  url: 'https://uploads0.wikiart.org/00339/images/leonardo-da-vinci/mona-lisa-c-1503-1519.jpg',
+  notes: 'Hiba Diba Hoppa Hoopa'
+});
+
+$ul.appendChild(entry);
 
 function submitEntryForm(event) {
   event.preventDefault();
@@ -38,4 +47,42 @@ function isValidUrl(urlString) {
     'i'
   );
   return !!urlPattern.test(urlString);
+}
+
+function createEntryElements(entry) {
+  var $li = elementCreator('li', { class: 'row' }, [
+    elementCreator('div', { class: 'column-half' }, [
+      elementCreator('img', {
+        class: 'entry-image image-shrink',
+        src: entry.url,
+        alt: `Entry Image ${entry.id}`
+      })
+    ]),
+    elementCreator('div', { class: 'column-half' }, [
+      elementCreator('h3', { innerText: entry.title }),
+      elementCreator('p', {
+        innerText: entry.notes
+      })
+    ])
+  ]);
+  return $li;
+}
+
+function elementCreator(tagname, attributes, children = []) {
+  var element = document.createElement(tagname);
+  if (attributes) {
+    for (var attribute in attributes) {
+      if (attribute === 'innerText') {
+        element.textContent = attributes[attribute];
+      } else {
+        element.setAttribute(attribute, attributes[attribute]);
+      }
+    }
+  }
+  if (children.length) {
+    children.forEach(child => {
+      element.appendChild(child);
+    });
+  }
+  return element;
 }
